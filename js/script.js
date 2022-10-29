632c886611a5cdbc45fc1210ed8c5a8d620bdb51
King sejong <86579707+Taemin1234@users.noreply.github.com>
2022-10-29 22:24:50 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 $(function () {
   webNavbar(); // 웹페이지 상단바
+  slickJs(); // 메인화면 슬라이드
 });
 
-function webNavbar() {}
+function webNavbar() {
+  let headSlide = $(".header-slide");
+
+  $(".wNav-btn").on("mouseover", function () {
+    headSlide.stop().slideDown();
+  });
+
+  headSlide.on("mouseleave", function () {
+    headSlide.stop().slideUp();
+  });
+}
+
+function slickJs() {
+  $(".main-slide-wrap").slick({
+    dots: true,
+    arrows: false,
+    dotsClass: "slick-bar",
+    adaptiveHeight: true,
+  });
+}
